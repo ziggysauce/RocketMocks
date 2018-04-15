ed9c27294bb2fc461415c10b1dbcc822d0a344d1
ziggysauce <dtn3423@gmail.com>
2018-04-15 10:39:05 -0400

    Finish phone. Finish intro section

---
+++ surpRize/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Intro from './layout/Intro';
+
+const Dashboard = () => (
+  <div className="home__wrapper">
+    <Intro />
+  </div>
+);
+
+export default Dashboard;
---
+++ surpRize/src/components/layout/phone/Phone.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Phone = () => (
+  <div className="phone-screen">
+    <div className="phone-screen__top">
+      <div>
+        <span><i className="fas fa-signal"></i></span>
+        <span><i className="fas fa-wifi"></i></span>
+      </div>
+      <div>
+        9:41 AM
+      </div>
+      <div>
+        <span><i className="fab fa-bluetooth-b"></i></span>
+        <span>100%</span>
+        <span><i className="fas fa-battery-full"></i></span>
+      </div>
+    </div>
+    <div className="phone-screen__main">
+      <div className="phone-screen__main--person">
+        <img src="https://preview.ibb.co/cRNxBn/ny_square.jpg" alt="profile picture"/>
+        <p>Daniel Nguyen</p>
+      </div>
+      <div className="phone-screen__main--social-media">
+        <a href="https://www.linkedin.com/in/dtn3423/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
+        <a href="https://github.com/ziggysauce" target="_blank"><i className="fab fa-github"></i></a>
+        <a href="https://codepen.io/ziggysauce/" target="_blank"><i className="fab fa-codepen"></i></a>
+        <a href="https://medium.com/@ziggysauce" target="_blank"><i className="fab fa-medium-m"></i></a>
+        <a href="https://drive.google.com/file/d/1VdoxUOacJ6HNdt8ZyIqjZT9s1QxdWzi1/view" target="_blank"><i className="fas fa-file-pdf"></i></a>
+      </div>
+      <div className="phone-screen__main--contact">
+        <h3>Phone</h3>
+        <p>248-659-2754</p>
+      </div>
+      <div className="phone-screen__main--contact">
+        <h3>Email</h3>
+        <p>dtn3423@gmail.com</p>
+      </div>
+      <div className="phone-screen__main--contact">
+        <h3>Portfolio</h3>
+        <p>danieltnguyen.com</p>
+      </div>
+    </div>
+    <div className="phone-screen__bottom">
+      <span>Left</span>
+      <span>Right</span>
+    </div>
+  </div>
+);
+
+export default Phone;
