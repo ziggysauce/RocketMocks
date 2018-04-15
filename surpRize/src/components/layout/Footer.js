2360ac79160fb1979a3c6bd0c9b61dcf962cdac1
ziggysauce <dtn3423@gmail.com>
2018-04-15 17:52:16 -0400

    start footer

--- surpRize/src/components/layout/Footer.js
+++ surpRize/src/components/layout/Footer.js
@@ -3,7 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const Footer = () => (
   <footer>
-    <div>This is the footer</div>
+    <div className="footer__divider">
+      <div className="point-bg-transparent--invert"></div>
+      <div className="point-bg-white--invert"></div>
+    </div>
+    <div className="footer-test">This is the footer</div>
   </footer>
 );
 
