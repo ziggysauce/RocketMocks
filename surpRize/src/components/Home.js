dd54c0617f69c915b96e01df8c6190fe5fd2b838
ziggysauce <dtn3423@gmail.com>
2018-04-16 00:30:08 -0400

    Mobile styles

--- surpRize/src/components/Home.js
+++ surpRize/src/components/Home.js
@@ -11,15 +11,37 @@ import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy,
 
 
 class Dashboard extends React.Component {
-  scrollTo(destination) {
+  state = {
+    top: true,
+    mobileNav: false,
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      console.log('scrolling');
+      window.scrollY  < 50 ? this.setState({ top: true }) : this.setState({ top: false });
+    });
+  }
+
+  scrollTo = (destination) => {
     let pos = document.querySelector(destination).getBoundingClientRect().y;
     scroll.scrollMore(pos);
+    this.state.mobileNav ? this.setState({ mobileNav: false }) : this.setState({ mobileNav: true });
+  }
+
+  handleMobileNav = () => {
+    console.log('been clicked');
+    this.state.mobileNav ? this.setState({ mobileNav: false }) : this.setState({ mobileNav: true });
   }
 
   render() {
     return (
       <div>
-        <Header scrollTo={this.scrollTo}/>
+        <Header 
+          scrollTo={this.scrollTo} 
+          state={this.state}
+          handleMobileNav={this.handleMobileNav}
+        />
         <div className="home__wrapper" id="about">
           <Intro scrollTo={this.scrollTo}/>
           <GeneralInfo scrollTo={this.scrollTo}/>
--- surpRize/src/components/layout/Header.js
+++ surpRize/src/components/layout/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
-const Header = (props) => (
-  <header className="navbar__wrapper">
-    <h1>surpRIZE</h1>
-    <div className="navbar__links">
-      <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#about">About</a>
-      <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#traits">Traits</a>
-      <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#work">Work</a>
-      <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#interest">Interest</a>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  const { top, mobileNav } = props.state;
+
+  return (
+    <header className={top ? "navbar__wrapper transparent" : "navbar__wrapper"}>
+      <h1>surpRIZE</h1>
+      <div className={top ? "navbar__links transparent" : "navbar__links"}>
+        <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#about">About</a>
+        <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#traits">Traits</a>
+        <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#work">Work</a>
+        <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#interest">Interest</a>
+      </div>
+      <button className={mobileNav ? "toggle-button-display" : (top ? "navbar__mobile--toggle navbar__mobile--white" : "navbar__mobile--toggle")} onClick={() => props.handleMobileNav()}><i className="fas fa-bars"></i></button>
+      <button className={mobileNav ? "navbar__mobile--toggle" : "toggle-button-display"} onClick={() => props.handleMobileNav()}><i className="fas fa-times"></i></button>
+      <div className={mobileNav ? "navbar__mobile" : "navbar__mobile--closed"}>
+        <Fade top>
+          <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#about">About</a>
+          <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#traits">Traits</a>
+          <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#work">Work</a>
+          <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#interest">Interest</a>
+        </Fade>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
